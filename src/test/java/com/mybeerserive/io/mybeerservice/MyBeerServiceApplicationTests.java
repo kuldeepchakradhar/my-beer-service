ad35156b82925e31b50dd2727362a82e819543af
Arun <arun@gocoop.com>
2020-05-01 13:59:05 +0530

    my beer service first commit

---
+++ src/test/java/com/mybeerserive/io/mybeerservice/MyBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybeerserive.io.mybeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
